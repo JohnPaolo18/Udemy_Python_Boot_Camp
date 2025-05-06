0b24e349645122ef309d3ff4218e4e0433d4f8c7
JohnPaolo19 <140373424+JohnPaolo18@users.noreply.github.com>
2025-05-05 23:37:01 -0600

    Day 4: Rock Paper Scissors Project

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +26,57 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+players_choice = input("What do you choose? 0 for Rock, 1 for Paper or 2 for Scissors\n")
+
+hands_list = ["0", "1", "2"]
+random_choice = random.choice(hands_list)
+
+if players_choice == "0" and random_choice == "1":
+    print(rock)
+    print("Computer Choose:")
+    print(paper)
+    print("You lose")
+
+elif players_choice == "0" and random_choice == "2":
+    print(rock)
+    print("Computer Choose:")
+    print(scissors)
+    print("You Win")
+elif players_choice == "0" and random_choice == "0":
+    print(rock)
+    print("Computer Choose:")
+    print(rock)
+    print("It's a tie")
+elif players_choice == "1" and random_choice == "1":
+    print(paper)
+    print("Computer Choose:")
+    print(paper)
+    print("It's a Tie")
+elif players_choice == "1" and random_choice == "2":
+    print(paper)
+    print("Computer Choose:")
+    print(scissors)
+    print("You lose")
+elif players_choice == "1" and random_choice == "0":
+    print(paper)
+    print("Computer Choose:")
+    print(rock)
+    print("You Win")
+elif players_choice == "2" and random_choice == "2":
+    print(scissors)
+    print("Computer Choose:")
+    print(scissors)
+    print("It's a Tie!")
+elif players_choice == "2" and random_choice == "1":
+    print(scissors)
+    print("Computer Choose:")
+    print(paper)
+    print("You Win")
+elif players_choice == "2" and random_choice == "0":
+    print(scissors)
+    print("Computer Choose:")
+    print(rock)
+    print("You lose")
+else:
+    print("Your choice was incorrect")
